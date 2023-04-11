688470e1727b31bfbad757e6dc0b08e8edc530b6
Oriana Apostoli <78334888+oapo1804@users.noreply.github.com>
2023-04-10 22:57:11 -0400

    editing nav bar

--- MyCar/src/app/app.module.ts
+++ MyCar/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,7 +22,8 @@ import { CarpageComponent } from './carpage/carpage.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, 
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
