73faf55ba271971081582a334d78e4e4cd00f6ad
connorhitchings <connorch@live.unc.edu>
2023-04-11 14:26:46 -0400

    Setting up env

---
+++ MyCar/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyCT9brm-XvGANnqIzJEXI6F_6Oc8ezvgWs",
+    authDomain: "mycar-259be.firebaseapp.com",
+    projectId: "mycar-259be",
+    storageBucket: "mycar-259be.appspot.com",
+    messagingSenderId: "548335650962",
+    appId: "1:548335650962:web:2068669be13b9bbd317e4b",
+    measurementId: "G-WCESG7Q7JY"
+  };
