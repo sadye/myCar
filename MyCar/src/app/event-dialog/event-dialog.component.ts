8fcc1a73e1895f41fd067f7822fb90c21ed27aa1
sadye <sadye@live.unc.edu>
2023-04-26 22:19:30 -0400

    lets go

--- MyCar/src/app/event-dialog/event-dialog.component.ts
+++ MyCar/src/app/event-dialog/event-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Event } from '../eventdetail/event';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { CarService } from '../cars/car.service';
+import { Task } from '../task/task';
 
 interface EventType {
   value: string;
@@ -14,11 +17,35 @@ interface EventType {
 })
 export class EventDialogComponent {
   private backupEvent: Event = {...this.data.event};
+  email: string = ""
+  cars: Task[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<EventDialogComponent>,
+    private service: CarService,
     @Inject(MAT_DIALOG_DATA) public data: EventDialogData
-  ) {}
+  ) {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        if (user.email) {
+          this.email = user.email
+          this.service.getCars(this.cars, this.email);
+        }
+        // ...
+      } else {
+        this.email = ""
+      }
+
+    });
+    
+  }
+
+  ngOnInit(){
+    
+  }
   types: EventType[] = [
     {value: 'maintenance-0', viewValue: 'Maintenance'},
     {value: 'accident-1', viewValue: 'Accident'},
